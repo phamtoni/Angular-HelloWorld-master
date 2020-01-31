ce691c56e83bbdc853cb1ca4ef232dc95f9fe0ce
phamtoni <50487603+phamtoni@users.noreply.github.com>
2020-01-31 11:20:40 +0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
     selector: 'app-root',
     template: `
-        <h1>edit in github </h1>
+        <h1>edit in github new</h1>
     `
 })
 export class AppComponent implements OnInit {
